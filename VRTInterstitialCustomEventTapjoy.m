c9e4d09dcaa146deea7e07f472ce18115a23cb9f
Scott McCoy <scotthmccoy@gmail.com>
2022-08-07 23:17:35 -0700

    feat: Adapter tested in VITA

--- VRTInterstitialCustomEventTapjoy.m
+++ VRTInterstitialCustomEventTapjoy.m
@@ -2,70 +2,87 @@
 #import "VRTInterstitialCustomEventTapjoy.h"
 
 //Dependencies
-@import TapjoySDKInterstitial;
+@import Tapjoy;
 
-
-@interface VRTInterstitialCustomEventTapjoy() <SMAInterstitialDelegate>
-@property SMAInterstitial *smaInterstitial;
+@interface VRTInterstitialCustomEventTapjoy() <TJPlacementDelegate, TJPlacementVideoDelegate>
+@property TJPlacement *tjPlacement;
 @end
 
 @implementation VRTInterstitialCustomEventTapjoy
 
 - (void) loadInterstitialAd {
-    NSString *adSpaceId = [self.customEventConfig.thirdPartyCustomEventData objectForKey:@"adUnitId"];
+    NSString *placementName = [self.customEventConfig.thirdPartyCustomEventData objectForKey:@"adUnitId"];
     
-    if (adSpaceId == nil) {
-        VRTError *error = [VRTError errorWithCode:VRTErrorCodeCustomEvent message:@"No adSpaceId"];
+    if (placementName == nil) {
+        VRTError *error = [VRTError errorWithCode:VRTErrorCodeCustomEvent message:@"No placementName"];
         [self.customEventLoadDelegate customEventFailedToLoadWithError:error];
         return;
     }
-
-    [TapjoySDK loadInterstitialForAdSpaceId:adSpaceId delegate:self];
+    
+    self.tjPlacement = [[TJPlacement alloc] init];
+    self.tjPlacement.placementName = placementName;
+    self.tjPlacement.delegate = self;
+    self.tjPlacement.videoDelegate = self;
+    [self.tjPlacement requestContent];
 }
 
 - (void) showInterstitialAd {
     UIViewController *vc = [self.viewControllerDelegate vrtViewControllerForModalPresentation];
-    [self.smaInterstitial showFromViewController:vc];
+    [self.tjPlacement showContentWithViewController:vc];
 }
 
 
-#pragma mark - VRTVungleManagerDelegate
-
-- (void)interstitialDidLoad:(SMAInterstitial *_Nonnull)interstitial {
-    self.smaInterstitial = interstitial;
-    [self.customEventLoadDelegate customEventLoaded];
-}
-
-- (void)interstitial:(SMAInterstitial *_Nullable)interstitial didFailWithError:(NSError *_Nonnull)error {
-    [self.customEventLoadDelegate customEventFailedToLoadWithError:error];
-}
-
-- (void)interstitialDidTTLExpire:(SMAInterstitial *_Nonnull)interstitial {
+#pragma mark - TJPlacementDelegate
+- (void)requestDidSucceed:(TJPlacement *)placement {
     //No VRT Analog
 }
 
-- (void)interstitialWillAppear:(SMAInterstitial *_Nonnull)interstitial {
-    [self.customEventShowDelegate customEventWillPresentModal:VRTModalTypeInterstitial];
+- (void)requestDidFail:(TJPlacement *)placement error:(nullable NSError *)error {
+    [self.customEventLoadDelegate customEventFailedToLoadWithError:error];
 }
 
-- (void)interstitialDidAppear:(SMAInterstitial *_Nonnull)interstitial {
+- (void)contentIsReady:(TJPlacement *)placement {
+    [self.customEventLoadDelegate customEventLoaded];
+}
+
+- (void)contentDidAppear:(TJPlacement *)placement {
     [self.customEventShowDelegate customEventDidPresentModal:VRTModalTypeInterstitial];
 }
 
-- (void)interstitialWillDisappear:(SMAInterstitial *_Nonnull)interstitial {
-    [self.customEventShowDelegate customEventWillDismissModal:VRTModalTypeInterstitial];
-}
-
-- (void)interstitialDidDisappear:(SMAInterstitial *_Nonnull)interstitial {
+- (void)contentDidDisappear:(TJPlacement *)placement {
     [self.customEventShowDelegate customEventDidDismissModal:VRTModalTypeInterstitial];
 }
 
-- (void)interstitialDidClick:(SMAInterstitial *_Nonnull)interstitial {
+- (void)didClick:(TJPlacement *)placement {
     [self.customEventShowDelegate customEventClicked];
 }
 
-- (void)interstitialWillLeaveApplication:(SMAInterstitial *_Nonnull)interstitial {
-    [self.customEventShowDelegate customEventWillLeaveApplication];
+- (void)placement:(TJPlacement *)placement
+didRequestPurchase:(nullable TJActionRequest *)request
+        productId:(nullable NSString *)productId {
+    //No VRT Analog
 }
+
+- (void)placement:(TJPlacement *)placement
+ didRequestReward:(nullable TJActionRequest *)request
+           itemId:(nullable NSString *)itemId
+         quantity:(int)quantity {
+    //No VRT Analog
+}
+
+#pragma mark - TJPlacementVideoDelegate
+- (void)videoDidStart:(TJPlacement *)placement {
+    [self.customEventShowDelegate customEventVideoStarted];
+}
+
+- (void)videoDidComplete:(TJPlacement *)placement {
+    [self.customEventShowDelegate customEventVideoCompleted];
+}
+
+- (void)videoDidFail:(TJPlacement *)placement error:(nullable NSString *)errorMsg {
+    VRTError *vrtError = [VRTError errorWithCode:VRTErrorCodeCustomEvent message:errorMsg];
+    [self.customEventLoadDelegate customEventFailedToLoadWithError:vrtError];
+}
+
 
 @end
