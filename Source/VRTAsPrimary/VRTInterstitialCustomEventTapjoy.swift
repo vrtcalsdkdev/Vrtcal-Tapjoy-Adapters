aa2da6590db9a95c49fecd39f0c6169418910a2d
Scott McCoy <scott.mccoy@vrtcal.com>
2024-07-15 18:44:40 -0700

    Update for TapJoy 14.0.0

--- Source/VRTAsPrimary/VRTInterstitialCustomEventTapjoy.swift
+++ Source/VRTAsPrimary/VRTInterstitialCustomEventTapjoy.swift
@@ -7,7 +7,6 @@ class VRTInterstitialCustomEventTapjoy: VRTAbstractInterstitialCustomEvent {
     private var tjPlacement: TJPlacement?
     
     var tjPlacementDelegatePassthrough = TJPlacementDelegatePassthrough()
-    var tjPlacementVideoDelegatePassthrough = TJPlacementVideoDelegatePassthrough()
     
     override func loadInterstitialAd() {
         VRTLogInfo()
@@ -18,8 +17,6 @@ class VRTInterstitialCustomEventTapjoy: VRTAbstractInterstitialCustomEvent {
         }
         VRTLogInfo("placementName: \(placementName)")
         
-        tjPlacementVideoDelegatePassthrough.customEventLoadDelegate = customEventLoadDelegate
-        
         tjPlacementDelegatePassthrough.customEventShowDelegate = customEventShowDelegate
         tjPlacementDelegatePassthrough.customEventLoadDelegate = customEventLoadDelegate
         tjPlacementDelegatePassthrough.viewControllerDelegate = viewControllerDelegate
@@ -27,14 +24,12 @@ class VRTInterstitialCustomEventTapjoy: VRTAbstractInterstitialCustomEvent {
         tjPlacement = TJPlacement()
         tjPlacement?.placementName = placementName
         tjPlacement?.delegate = tjPlacementDelegatePassthrough
-        tjPlacement?.videoDelegate = tjPlacementVideoDelegatePassthrough
         tjPlacement?.requestContent()
     }
     
     override func showInterstitialAd() {
         VRTLogInfo()
         tjPlacementDelegatePassthrough.customEventShowDelegate = customEventShowDelegate
-        tjPlacementVideoDelegatePassthrough.customEventShowDelegate = customEventShowDelegate
         let vc = viewControllerDelegate?.vrtViewControllerForModalPresentation()
         tjPlacement?.showContent(with: vc)
     }
